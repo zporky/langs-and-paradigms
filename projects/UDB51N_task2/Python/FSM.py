e199e970bcab36249cf2fabe76cceb06653ca4c0
violaniko <violaniko@gmail.com>
2015-05-04 21:04:20 +0200

    Create FSM.py

---
+++ projects/UDB51N_task2/Python/FSM.py
@@ -0,0 +1,110 @@
+import sys
+
+########
+# Defining custom exception
+########
+class InvalidTransationException(Exception):
+    pass
+
+########
+# Defining states
+########
+states = {}
+states['START'] = False;
+states['S_'] = False;
+states['I0'] = False;
+states['T1'] = True;
+states['T2'] = True;
+states['T3'] = True;
+states['T4'] = True;
+states['T5'] = True;
+states['T6'] = True;
+
+states['I1'] = False;
+states['I2'] = False;
+states['T7'] = True;
+
+########
+# Defining transitions
+########
+transitions = {}
+transitions['s'] = ['+', '-']
+transitions['p'] = ['.']
+transitions['z'] = ['0']
+transitions['d'] = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+transitions['e'] = ['e','E']
+
+def getTransation(char):
+    for x in transitions:
+        if char in transitions[x]:
+            return x
+    raise InvalidTransationException("Input is not in alphabet.")
+
+########
+# Defining production rules
+########
+
+productionRules = {}
+productionRules['START', 'p'] = 'I0'
+productionRules['START', 'z'] = 'T2'
+productionRules['START', 'd'] = 'T4'
+productionRules['START', 's'] = 'S_'
+ 
+productionRules['S_', 'p'] = 'I0'
+productionRules['S_', 'z'] = 'T2'
+productionRules['S_', 'd'] = 'T4'
+
+productionRules['I0', 'd'] = 'T1'
+productionRules['T2', 'p'] = 'T3'
+productionRules['T4', 'p'] = 'T5'
+productionRules['T4', 'd'] = 'T4'
+
+productionRules['T1', 'd'] = 'T1'
+productionRules['T3', 'd'] = 'T3'
+productionRules['T5', 'd'] = 'T5'
+
+
+productionRules['T1', 'e'] = 'I1'
+productionRules['T3', 'e'] = 'I1'
+productionRules['T4', 'e'] = 'I1'
+productionRules['T5', 'e'] = 'I1'
+
+productionRules['I1', 'd'] = 'T7'
+productionRules['I1', 's'] = 'I2'
+productionRules['I2', 'd'] = 'T7'
+
+def getNextState(currentState, transation):
+    try:
+        return productionRules[currentState, transation]
+    except KeyError:
+        raise InvalidTransationException(currentState + ' state does not accept transition ' + transation)
+
+########
+# validateInput
+########
+def validateInput(line):
+    currentState = 'START';
+    for c in line:
+        transation = getTransation(c);
+        currentState = getNextState(currentState, transation)
+    if not states[currentState]: #if not in ending state 
+        raise InvalidTransationException('Premature end of line')
+
+########
+# processLine
+########
+def processLine(line):
+    'Gives descriptive information about the input'
+    try:
+        validateInput(line)
+        print ("OK " + line)
+    except InvalidTransationException:
+        print ("FAIL " + line)
+
+########
+# The "Main" "method"
+########
+lines = [line.strip() for line in open(sys.argv[1])]
+for line in lines:
+    processLine(line) 
