c822c015a4ee593443e28de11654db7af6aa1537
neataai <neataai@caesar.elte.hu>
2015-05-08 11:24:12 +0200

    Implement task2.

--- projects/ULFLX1/erlang/double.erl
+++ projects/ULFLX1/erlang/double.erl
@@ -6,17 +6,18 @@
 -define(STOP, {stop, normal, aborted, []}).
 -define(STOP(S), {stop, normal, S, []}).
 -define(IS_DIGIT(C), C >= $0 andalso C =< $9).
+-define(IS_EXP(C), C == $e orelse C == $E).
 -define(IS_TERMINAL(State), hd(atom_to_list(State)) == $t).
 -define(INITIAL_STATE, s).
  
 -export([parse/1, test/0]).
 %% gen_fsm callbacks - should not be called directly
 -export([init/1, handle_sync_event/4, terminate/3,
-        s/3, s_/3, i0/3, t1/3, t2/3, t3/3, t4/3, t5/3]).
+        s/3, s_/3, i0/3, t1/3, t2/3, t3/3, t4/3, t5/3, i6/3, t7/3]).
 
 test() ->
     PositiveTests = ["1", "-3.14","0", "+0.1", ".1", "1"],
-    NegativeTests = [".", "+ x1", "-", "3.14e-2"],
+    NegativeTests = [".", "+ x1", "-", "3.14e-2", "1e2", "2.4E-5", ".1e+3"],
     Result =[parse(Str) || Str <- PositiveTests] ++
         [parse(Str) || Str <- NegativeTests],
     io:format("~p~n", [Result]).
@@ -65,17 +66,29 @@ i0(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t1, D ++ [C]);
 i0(_, _, _) -> ?STOP.
 
 t1(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t1, D ++ [C]);
+t1(C, _, D) when ?IS_EXP(C) -> ?REPLY(i6, D ++ [C]);
 t1(_, _, _) -> ?STOP.
 
 t2($., _, D) -> ?REPLY(t3, D ++ ".");
 t2(_, _, _) -> ?STOP.
 
 t3(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t3, D ++ [C]);
+t3(C, _, D) when ?IS_EXP(C) -> ?REPLY(i6, D ++ [C]);
 t3(_, _, _) -> ?STOP.
 
 t4($., _, D) -> ?REPLY(t5, D ++ ".");
 t4(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t4, D ++ [C]);
+t4(C, _, D) when ?IS_EXP(C) -> ?REPLY(i6, D ++ [C]);
 t4(_, _, _) -> ?STOP.
 
 t5(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t5, D ++ [C]);
+t5(C, _, D) when ?IS_EXP(C) -> ?REPLY(i6, D ++ [C]);
 t5(_, _, _) -> ?STOP.
+
+i6($- = C, _, D) -> ?REPLY(i6, D ++[C]);
+i6($+, _, D) -> ?REPLY(i6, D);
+i6(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t7, D ++ [C]);
+i6(_, _, _) -> ?STOP.
+
+t7(C, _, D) when ?IS_DIGIT(C) -> ?REPLY(t7, D ++ [C]);
+t7(_, _, _) -> ?STOP.
